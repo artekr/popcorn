500f4dd3a84730c8b5d405e27294ef73908aef47
Heng Wang <artekr.heng@gmail.com>
2017-11-20 21:11:02 -0500

    Adde right sidebar to app main container

---
+++ src/app/shared/layout/sidebar-right/sidebar-right.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'layout-sidebar-right',
+  templateUrl: './sidebar-right.component.html',
+  styleUrls: ['./sidebar-right.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class SidebarRightComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
