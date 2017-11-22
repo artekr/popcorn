3e2d2571b0db3ab9bcb2dff70a4baede4e03dce7
Heng Wang <artekr.heng@gmail.com>
2017-11-21 23:42:11 -0500

    Updated entry model with mock data

--- src/app/mock-entries.ts
+++ src/app/mock-entries.ts
@@ -1,6 +1,36 @@
 import { Entry } from './entries';
 
 export const ENTRIES: Entry[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' }
+  { 
+    id: 1, 
+    name: '撸铁',
+    definition: '健身。比撸管更健康的自娱方式🧐😣',
+    example: '青涩少年撸铁13年巨变，俘获超性感健身女友。',
+    status: 'VALID',
+    create_time: '2017-09-23T21:21:15.508-04:00[UTC-04:00]'
+  },
+  { 
+    id: 2,
+    name: '鸡地屁',
+    definition: 'GDP的谐音，中文为“国内生产总值”。最常见的衡量经济增长的指标。',
+    example: '我大天朝鸡地屁从先秦开始独领风骚两千年',
+    status: 'VALID',
+    create_time: '2017-09-23T21:21:15.508-04:00[UTC-04:00]'
+  },
+  { 
+    id: 3,
+    name: '帮主',
+    definition: '同“乔帮主”，指篮球之神Michael Jordan。因为Jordan中译乔丹，又跟天龙八部中的乔峰桥帮主一样厉害。所以被称为帮主。',
+    example: '要说拉杆上篮，帮主真不是针对谁。',
+    status: 'VALID',
+    create_time: '2017-09-23T21:21:15.508-04:00[UTC-04:00]'
+  },
+  { 
+    id: 4,
+    name: 'B站',
+    definition: '国内知名视频网站Bilibili 也被称为哔哩哔哩、bilibili弹幕网，或简称为B站，是中国大陆一个ACG相关的弹幕视频分享网站，为上海东方传媒（集团）有限公司旗下网站，其前身为视频分享网站Mikufans。该网站由原AcFun网友“⑨bishi”（徐逸）于2009年6月26日创建。由于AcFun网站在运行时往往不稳定，Mikufans建站的初衷是为用户提供一个稳定的弹幕视频分享网站。网站于2010年1月24日更名为bilibili，和AcFun也存在竞争关系，不过后者比较集中在动画内容，缺少ACG的G，即游戏内容。',
+    example: '',
+    status: 'VALID',
+    create_time: '2017-09-23T21:21:15.508-04:00[UTC-04:00]'
+  }
 ]
