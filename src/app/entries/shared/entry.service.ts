d1c39b35f319300c402eb4d7ea135e33945c8170
Heng Wang <artekr.heng@gmail.com>
2017-11-23 01:07:11 -0500

    Hook up with backend server to query hot entries

--- src/app/entries/shared/entry.service.ts
+++ src/app/entries/shared/entry.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
@@ -9,10 +10,12 @@ import { ENTRIES } from '../../mock-entries';
 @Injectable()
 export class EntryService {
 
-  constructor() { }
+  private hotEntriesUrl = 'http://13.124.84.160:4321/entry/hot_entry';
+
+  constructor(private http: HttpClient) { }
 
   query(): Observable<Entry[]> {
-    return of(ENTRIES);
+    return this.http.get<Entry[]>(this.hotEntriesUrl);
   }
 
 }
