6f7bc31914e2becb383c894c0bf261d8920f1caf
Heng Wang <kevinwang264@gmail.com>
2017-11-23 23:50:52 -0500

    Updated entry component to include tag

--- src/app/entries/shared/entry.model.ts
+++ src/app/entries/shared/entry.model.ts
@@ -1,8 +1,11 @@
+import { Tag } from "./tag.model";
+
 export class Entry {
     id: number;
-    entry_name: string; //TODO: changed to 'name' in v3
+    name: string; //TODO: changed to 'name' in v3
     definition: string;
     example: string;
     status: string;
     create_time: string;
+    tags: Array<Tag> = [];
 }
