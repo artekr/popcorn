7e432459a7bbfa11528566e380df74aef9e1c8e6
Heng Wang <kevinwang264@gmail.com>
2017-11-26 19:02:49 -0500

    Added Post Put and Delete Http method to Api Service

--- src/app/core/api.service.ts
+++ src/app/core/api.service.ts
@@ -49,4 +49,35 @@ export class ApiService {
       )
   }
 
+  post(path: string, body: Object = {}): Observable<any> {
+    return this.http.post(
+      `${environment.api_url}${path}`,
+      JSON.stringify(body),
+      { headers: this.setHeaders() }
+    )
+    .pipe(
+      catchError(this.formatErrors)
+    )
+  }
+
+  put(path: string, body: Object = {}): Observable<any> {
+    return this.http.put(
+      `${environment.api_url}${path}`,
+      JSON.stringify(body),
+      { headers: this.setHeaders() }
+    )
+    .pipe(
+      catchError(this.formatErrors)
+    )
+  }
+
+  delete(path: string): Observable<any> {
+    return this.http.delete(
+      `${environment.api_url}${path}`,
+      { headers: this.setHeaders() }
+    )
+    .pipe(
+      catchError(this.formatErrors)
+    )
+  }
 }
