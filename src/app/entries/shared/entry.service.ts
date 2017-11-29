f1e09bfa62aadfb951db16150ecbe323a6cde101
Heng Wang <kevinwang264@gmail.com>
2017-11-29 11:18:29 -0500

    Use mock data for testing

--- src/app/entries/shared/entry.service.ts
+++ src/app/entries/shared/entry.service.ts
@@ -6,17 +6,20 @@ import { of } from 'rxjs/observable/of';
 import { Entry } from './entry.model';
 import { ApiService } from '../../core/api.service';
 
+import { ENTRIES } from "../../mock-entries";
+
 @Injectable()
 export class EntryService {
 
   constructor(
-    private apiService: ApiService  
+    private apiService: ApiService
   ) {}
 
   query(): Observable<Entry[]> {
-    return this.apiService.get(
-      '/entry/hot_entry'
-    );
+    return of(ENTRIES);
+    // return this.apiService.get(
+    //   '/entry/hot_entry'
+    // );
   }
 
 }
