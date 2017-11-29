683bbe94f435002a828d67d819d674cab96c7249
Heng Wang <kevinwang264@gmail.com>
2017-11-29 10:42:44 -0500

    Added Token service

---
+++ src/app/core/token.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class TokenService {
+  constructor() { }
+
+  getToken(): string {
+    return '';
+  }
+
+  saveToken(token: string) {
+    
+  }
+
+  destroyToken() {
+    
+  }
+}
