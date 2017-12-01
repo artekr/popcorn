9e3abde909726a2b182c481ac6f057d61dc3193f
Heng Wang <kevinwang264@gmail.com>
2017-12-01 10:57:38 -0500

    Added show auth directive

---
+++ src/app/core/auth/show-authed.directive.ts
@@ -0,0 +1,40 @@
+import { 
+  Directive,
+  Input,
+  OnInit,
+  TemplateRef,
+  ViewContainerRef
+} from '@angular/core';
+
+import { UserService } from '../user.service';
+
+@Directive({
+  selector: '[showAuthed]'
+})
+export class ShowAuthedDirective implements OnInit {
+
+  constructor(
+    private templateRef   : TemplateRef<any>,
+    private userService   : UserService,
+    private viewContainer : ViewContainerRef
+  ) {}
+
+  condition: boolean;
+
+  ngOnInit() {
+    this.userService.isAuthenticated.subscribe(
+      (isAuthenticated) => {
+        if (isAuthenticated && this.condition || !isAuthenticated && !this.condition) {
+          this.viewContainer.createEmbeddedView(this.templateRef);
+        } else {
+          this.viewContainer.clear();
+        }
+      }
+    )
+  }
+
+  @Input() set showAuthed(condition: boolean) {
+    this.condition = condition;
+  }
+
+}
