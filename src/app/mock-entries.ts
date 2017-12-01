1b77e2626fbf8953dc7b274138b738de4998eed8
Heng Wang <kevinwang264@gmail.com>
2017-11-30 19:13:16 -0500

    Updated mock data

--- src/app/mock-entries.ts
+++ src/app/mock-entries.ts
@@ -19,6 +19,180 @@ export const ENTRIES: Entry[] = [
         "name": "网络",
         "status": "VALID"
       }
+    ],
+    "comments": [
+      {
+        "id": 3,
+        "content": "哈哈哈哈",
+        "status": "VALID",
+        "create_time": "2017-09-23T21:22:01.499-04:00[UTC-04:00]",
+        "create_user": {
+          "id": 4,
+          "username": "Heng",
+          "email": "qihua@gmail.com",
+          "bio": "xxxx xxxx xxxx xxxx",
+          "status": "VALID",
+          "create_time": "2017-09-23T21:20:56.242-04:00[UTC-04:00]",
+          "update_time": "2017-09-23T21:20:56.242-04:00[UTC-04:00]"
+        },
+        "update_time": "2017-09-23T21:22:01.499-04:00[UTC-04:00]",
+        "update_user": {
+          "id": 4,
+          "username": "Heng",
+          "email": "qihua@gmail.com",
+          "bio": "xxxx xxxx xxxx xxxx",
+          "status": "VALID",
+          "create_time": "2017-09-23T21:20:56.242-04:00[UTC-04:00]",
+          "update_time": "2017-09-23T21:20:56.242-04:00[UTC-04:00]"
+        }
+      },
+      {
+          "id": 2,
+          "content": "纳尼",
+          "status": "VALID",
+          "create_time": "2017-09-23T21:21:49.084-04:00[UTC-04:00]",
+          "create_user": {
+              "id": 3,
+              "username": "Yang",
+              "email": "qihua@gmail.com",
+              "bio": "xxxx xxxx xxxx xxxx",
+              "status": "VALID",
+              "create_time": "2017-09-23T21:20:50.853-04:00[UTC-04:00]",
+              "update_time": "2017-09-23T21:20:50.853-04:00[UTC-04:00]"
+          },
+          "update_time": "2017-09-23T21:21:49.084-04:00[UTC-04:00]",
+          "update_user": {
+              "id": 3,
+              "username": "Yang",
+              "email": "qihua@gmail.com",
+              "bio": "xxxx xxxx xxxx xxxx",
+              "status": "VALID",
+              "create_time": "2017-09-23T21:20:50.853-04:00[UTC-04:00]",
+              "update_time": "2017-09-23T21:20:50.853-04:00[UTC-04:00]"
+          }
+      },
+      {
+          "id": 1,
+          "content": "啥子东西",
+          "status": "VALID",
+          "create_time": "2017-09-23T21:21:37.661-04:00[UTC-04:00]",
+          "create_user": {
+              "id": 2,
+              "username": "Qihua",
+              "email": "qihua@gmail.com",
+              "bio": "xxxx xxxx xxxx xxxx",
+              "status": "VALID",
+              "create_time": "2017-09-23T21:20:45.652-04:00[UTC-04:00]",
+              "update_time": "2017-09-23T21:20:45.652-04:00[UTC-04:00]"
+          },
+          "update_time": "2017-09-23T21:21:37.662-04:00[UTC-04:00]",
+          "update_user": {
+              "id": 2,
+              "username": "Qihua",
+              "email": "qihua@gmail.com",
+              "bio": "xxxx xxxx xxxx xxxx",
+              "status": "VALID",
+              "create_time": "2017-09-23T21:20:45.652-04:00[UTC-04:00]",
+              "update_time": "2017-09-23T21:20:45.652-04:00[UTC-04:00]"
+          }
+      }
+    ]
+  },
+  {
+    "id": 2,
+    "name": "李菊福",
+    "definition": "有理有据，令人信服",
+    "example": "楼主李菊福",
+    "status": "VALID",
+    "create_time": "2017-09-23T21:21:15.508-04:00[UTC-04:00]",
+    "tags": [
+      {
+        "id": 3,
+        "name": "网络",
+        "status": "VALID"
+      },
+      {
+        "id": 4,
+        "name": "论坛",
+        "status": "VALID"
+      }
+    ],
+    "comments": [
+      {
+        "id": 4,
+        "content": "哈哈哈哈",
+        "status": "VALID",
+        "create_time": "2017-09-23T21:22:01.499-04:00[UTC-04:00]",
+        "create_user": {
+          "id": 4,
+          "username": "Heng",
+          "email": "qihua@gmail.com",
+          "bio": "xxxx xxxx xxxx xxxx",
+          "status": "VALID",
+          "create_time": "2017-09-23T21:20:56.242-04:00[UTC-04:00]",
+          "update_time": "2017-09-23T21:20:56.242-04:00[UTC-04:00]"
+        },
+        "update_time": "2017-09-23T21:22:01.499-04:00[UTC-04:00]",
+        "update_user": {
+          "id": 4,
+          "username": "Heng",
+          "email": "qihua@gmail.com",
+          "bio": "xxxx xxxx xxxx xxxx",
+          "status": "VALID",
+          "create_time": "2017-09-23T21:20:56.242-04:00[UTC-04:00]",
+          "update_time": "2017-09-23T21:20:56.242-04:00[UTC-04:00]"
+        }
+      },
+      {
+          "id": 5,
+          "content": "纳尼",
+          "status": "VALID",
+          "create_time": "2017-09-23T21:21:49.084-04:00[UTC-04:00]",
+          "create_user": {
+              "id": 3,
+              "username": "Yang",
+              "email": "qihua@gmail.com",
+              "bio": "xxxx xxxx xxxx xxxx",
+              "status": "VALID",
+              "create_time": "2017-09-23T21:20:50.853-04:00[UTC-04:00]",
+              "update_time": "2017-09-23T21:20:50.853-04:00[UTC-04:00]"
+          },
+          "update_time": "2017-09-23T21:21:49.084-04:00[UTC-04:00]",
+          "update_user": {
+              "id": 3,
+              "username": "Yang",
+              "email": "qihua@gmail.com",
+              "bio": "xxxx xxxx xxxx xxxx",
+              "status": "VALID",
+              "create_time": "2017-09-23T21:20:50.853-04:00[UTC-04:00]",
+              "update_time": "2017-09-23T21:20:50.853-04:00[UTC-04:00]"
+          }
+      },
+      {
+          "id": 6,
+          "content": "啥子东西",
+          "status": "VALID",
+          "create_time": "2017-09-23T21:21:37.661-04:00[UTC-04:00]",
+          "create_user": {
+              "id": 2,
+              "username": "Qihua",
+              "email": "qihua@gmail.com",
+              "bio": "xxxx xxxx xxxx xxxx",
+              "status": "VALID",
+              "create_time": "2017-09-23T21:20:45.652-04:00[UTC-04:00]",
+              "update_time": "2017-09-23T21:20:45.652-04:00[UTC-04:00]"
+          },
+          "update_time": "2017-09-23T21:21:37.662-04:00[UTC-04:00]",
+          "update_user": {
+              "id": 2,
+              "username": "Qihua",
+              "email": "qihua@gmail.com",
+              "bio": "xxxx xxxx xxxx xxxx",
+              "status": "VALID",
+              "create_time": "2017-09-23T21:20:45.652-04:00[UTC-04:00]",
+              "update_time": "2017-09-23T21:20:45.652-04:00[UTC-04:00]"
+          }
+      }
     ]
   }
 ]
