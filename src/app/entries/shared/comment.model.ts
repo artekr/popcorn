7878b03aae4c35fa138b06bda75edd33cdfbb650
Heng Wang <kevinwang264@gmail.com>
2017-11-30 19:12:28 -0500

    Added Comment model

---
+++ src/app/entries/shared/comment.model.ts
@@ -0,0 +1,11 @@
+import { User } from "../../core/user.model";
+
+export class Comment {
+    id          : number;
+    content     : string;
+    status      : string;
+    create_time : string;
+    create_user : User;
+    update_time : string;
+    update_user : User
+}
--- src/app/entries/shared/entry.model.ts
+++ src/app/entries/shared/entry.model.ts
@@ -1,11 +1,13 @@
 import { Tag } from "./tag.model";
+import { Comment } from "./comment.model";
 
 export class Entry {
-    id: number;
-    name: string; //TODO: changed to 'name' in v3
-    definition: string;
-    example: string;
-    status: string;
-    create_time: string;
-    tags: Array<Tag> = [];
+    id          : number;
+    name        : string; //TODO: changed to 'name' in v3
+    definition  : string;
+    example     : string;
+    status      : string;
+    create_time : string;
+    tags        : Array<Tag> = [];
+    comments    : Array<Comment> = [];
 }
