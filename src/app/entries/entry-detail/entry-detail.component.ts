50eae4e76f2ac5f9c749aa0a462984a9c586320f
Heng Wang <kevinwang264@gmail.com>
2017-11-30 19:13:05 -0500

    Added entry detail component

---
+++ src/app/entries/entry-detail/entry-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-entry-detail',
+  templateUrl: './entry-detail.component.html',
+  styleUrls: ['./entry-detail.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class EntryDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
