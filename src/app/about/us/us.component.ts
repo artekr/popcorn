b921e9220eed58623b597d3fdec695c8681c6a27
Heng Wang <kevinwang264@gmail.com>
2017-12-02 01:25:50 -0500

    Added about us component

---
+++ src/app/about/us/us.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-us',
+  templateUrl: './us.component.html',
+  styleUrls: ['./us.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class UsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
