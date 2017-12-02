e5c124844ec305c0134f6c5b8d8cb3feb275f80a
Heng Wang <kevinwang264@gmail.com>
2017-12-02 01:25:23 -0500

    Added disclaimer component

---
+++ src/app/about/disclaimer/disclaimer.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-disclaimer',
+  templateUrl: './disclaimer.component.html',
+  styleUrls: ['./disclaimer.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class DisclaimerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
