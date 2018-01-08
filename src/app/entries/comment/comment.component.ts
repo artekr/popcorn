cc8c337a235614619d9feb0d7cf00bdfc02c2d7d
Heng Wang <kevinwang264@gmail.com>
2018-01-08 00:52:26 -0500

    feat: Delete a comment by logged in user

--- src/app/entries/comment/comment.component.ts
+++ src/app/entries/comment/comment.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewEncapsulation, Input, Pipe, PipeTransform } from '@angular/core';
 
+import { CookieService } from 'ngx-cookie-service';
+
 import { Comment } from '../shared/comment.model';
 import { ApiService } from '../../core/api.service';
 
@@ -14,12 +16,30 @@ export class CommentComponent implements OnInit {
   @Input() entry_id: number;
 
   private comment: string = "";
+  private currentUserId: string = "";
 
   constructor(
-    private apiService: ApiService
+    private apiService: ApiService,
+    private cookieService: CookieService
   ) {}
 
   ngOnInit() {
+    if (this.cookieService.check('userid')) {
+      this.currentUserId = this.cookieService.get('userid');
+    }
+  }
+
+  onDeleteComment(comment_id: number, comment: Comment) {
+    this.apiService.delete('/entries/' + String(this.entry_id) + '/comments/' + String(comment_id))
+        .subscribe(
+          data => {
+            this.comments.splice(this.comments.indexOf(comment), 1);
+          },
+          error => {
+            // TODO: error handling
+            console.log("error " + error);
+          }
+        );
   }
 
   onSubmitComment() {
