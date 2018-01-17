43e0726366a3810d4ec5255e4ee5fdc074cd9b17
Heng Wang <kevinwang264@gmail.com>
2018-01-17 00:45:06 -0500

    feat: Searching suggestion working!

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -7,6 +7,7 @@ import { AddEntryComponent } from '../add-entry/add-entry.component';
 import { User } from '../../../core/user.model';
 import { UserService } from '../../../core/user.service';
 import { AlertService } from '../../services';
+import { environment } from '../../../../environments/environment';
 
 declare var $: any;
 
@@ -39,6 +40,64 @@ export class HeaderComponent implements OnInit {
     if (this.cookieService.check("username")) {
       this.username = this.cookieService.get("username");
     }
+
+    this.searchInit();
+  }
+
+  searchInit() {
+    $('.ui.search')
+      .search({
+        // type       : 'category',
+        minCharacters : 3,
+        apiSettings   : {
+          onResponse: function(searchResponse) {
+            /*
+            // For category usage
+            var response = {
+              results : []
+            };
+            */
+            var response = {
+                results : []
+              };
+            // translate API response to work with search
+            $.each(searchResponse, function(index, item) {
+              var
+                type       = '词条',
+                maxResults = 8
+              ;
+              if(index >= maxResults) {
+                return false;
+              }
+              /*
+              // create new type category
+              if(response.results[type] === undefined) {
+                response.results[type] = {
+                  name    : type,
+                  results : []
+                };
+              }
+              // add result to category
+              response.results[type].results.push({
+                title : item.name,
+              });
+              */
+              response.results.push({
+                title : item.name,
+                // TODO: replace proper url
+                url: 'http://bopucidian.com/entry/id/'+item.id
+              });
+            });
+            return response;
+          },
+          url: `${environment.api_url}`+'/entries/suggestions/{query}',
+          headers: {
+            'contentType': 'application/json',
+            'Accept'     : 'application/json',
+            'time_zone'  : 'UTC-05:00'
+          },
+        }
+      });
   }
 
   showAddEntryModal() {
