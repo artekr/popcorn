f3a05bc2022b6ba4499761e7b58d6c7a44ae1e50
Heng Wang <kevinwang264@gmail.com>
2018-01-20 00:31:24 -0500

    feat: Dynamically load different sidebar component based on the type passed on

--- src/app/shared/layout/sidebar-right/sidebar-right.component.ts
+++ src/app/shared/layout/sidebar-right/sidebar-right.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+
+import { SidebarComponentType } from '../../model';
 
 @Component({
   selector: 'layout-sidebar-right',
@@ -8,9 +10,37 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class SidebarRightComponent implements OnInit {
   
-  constructor() { }
+  @Input() childComponentType : SidebarComponentType[];
+  
+  private showHotTags: boolean        = false;
+  private showRelatedTags: boolean    = false;
+  private showRelatedEntries: boolean = false;
+
+  constructor() {}
 
   ngOnInit() {
+    if (this.childComponentType) {
+      for (let type of this.childComponentType) {
+        switch(type) {
+          case SidebarComponentType.HotTags: {
+            this.showHotTags = true;
+            break;
+          }
+          case SidebarComponentType.RelatedTags: {
+            this.showRelatedTags = true;
+            break;
+          }
+          case SidebarComponentType.RelatedEntries: {
+            this.showRelatedEntries = true;
+            break;
+          }
+          default: {
+            this.showHotTags = true;
+            break;
+          }
+        }
+      }
+    }
   }
 
 }
