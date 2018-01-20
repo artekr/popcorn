e9338005fe5efd543e05b84a9e389d825f3dc18f
Heng Wang <kevinwang264@gmail.com>
2018-01-20 00:32:18 -0500

    Load related entries component on sidebar in /entry URL

--- src/app/entries/entries.component.ts
+++ src/app/entries/entries.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
+import { SidebarComponentType } from '../shared/model';
+
 @Component({
   selector: 'app-entries',
   templateUrl: './entries.component.html',
@@ -8,9 +10,12 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class EntriesComponent implements OnInit {
 
+  private relatedEntries: SidebarComponentType[];
+
   constructor() { }
 
   ngOnInit() {
+    this.relatedEntries = [SidebarComponentType.RelatedEntries];
   }
 
 }
